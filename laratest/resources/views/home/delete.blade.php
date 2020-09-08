b3eed8df9220fea59613b8c47a86374f0d8af9ef
Towsif824 <tawsif824@gmail.com>
2020-09-08 13:55:47 +0600

    finalCommit

--- laratest/resources/views/home/delete.blade.php
+++ laratest/resources/views/home/delete.blade.php
@@ -11,18 +11,15 @@
 		@csrf
 		<table>
 			<tr>
-				<td>Name</td>
-				<td>{{$user['name']}}</td>
+				<td>UserName</td>
+				<td>{{$user['username']}}</td>
 			</tr>
 
 			<tr>
 				<td>Password</td>
 				<td>{{$user['password']}}</td>
 			</tr>
-			<tr>
-				<td>Email</td>
-				<td>{{$user['email']}}</td>
-			</tr>
+			
 		</table>
 		<h3>Are you sure?</h3>
 		<input type="submit" name="submit" value="Confirm">
